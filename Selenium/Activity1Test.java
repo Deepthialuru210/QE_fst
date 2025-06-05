42c7039e59b8569d8ed00812c62edd57fe22d659
Deepthialuru210 <deepthi2.aluru10@gmail.com>
2025-06-05 09:51:56 +0530

    Add files via upload

---
+++ Selenium/Activity1Test.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class Activity1Test {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new FirefoxDriver();
+        
+    }
+
+    @Test
+    public void OpenTS() {
+    	driver.get("https://training-support.net");
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+        
+    }
+
+    @AfterClass
+    public void Close() {
+     driver.close();
+    }
+}
